e10048f031d3b35ed4a3ee4d52d741ce207319a6
OlevanA <ole.allen@inn.no>
2025-02-06 20:13:22 +0100

    many small changes

--- data/processed/Fix_endowments_two.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load URR file
-df = pd.read_csv("C:/Users/oleva/PycharmProjects/MODPET/data/processed/Oil_estimate_sorted.csv")
-
-# Print available estimates
-print("Available URR keys:", df["estimate"].tolist())
--- data/raw/Check file.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("sorted_oil_endowments_with_estimations.csv")
-print(df.head())  # Shows the first few rows
-print(df.columns)  # Shows column names
